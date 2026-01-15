294b6eb01c7958ab17e2ee2af3c78f43f5a24ee4
Yangmingkai Li <157403552+ymkli@users.noreply.github.com>
2026-01-16 02:34:31 +0800

    Add files via upload

--- Scripts/Figure_3_exceedance_days.R
+++ Scripts/Figure_3_exceedance_days.R
@@ -12,26 +12,7 @@ data_path <- "../Data"
 data_base <- readRDS(paste0(data_path, "/EPA_station_TaUV_SO.rds")) %>%
   select(lon, lat, date, baseline_o3)
 
-data <- readRDS(paste0(data_path, "/EPA_Station_2006_2023_daily_O3.rds")) %>%
-  left_join(data_base) %>%
-  mutate(
-    baseline_o3 = ifelse(is.na(baseline_o3), 0, baseline_o3),
-    year = year(date),
-    month = month(date),
-    location = ifelse(lon < -100, "Western US", "Eastern US"),
-    location = factor(location, levels = c("Western US", "Eastern US"))
-    ) %>%
-
-  group_by(location, year) %>%
-  summarise(
-    day_num = n(),
-    smoke_exceedance = sum(smokePM_pred > 0 & o3_mda8_ppb > 70 & baseline_o3 < 70),
-    nonsmoke_exceedance = sum(smokePM_pred <= 0 & o3_mda8_ppb > 70) + sum(smokePM_pred > 0 & o3_mda8_ppb > 70 & baseline_o3 > 70),
-    smoke_driven = smoke_exceedance/day_num*100,
-    nonsmoke_driven = nonsmoke_exceedance/day_num*100
-    ) %>%
-  ungroup() %>%
-  
+data <- readRDS(paste0(data_path, "/EPA_O3_exceedance_day.rds")) %>%
   pivot_longer(
     cols = c(smoke_driven, nonsmoke_driven),
     names_to = "type",
@@ -41,7 +22,7 @@ data <- readRDS(paste0(data_path, "/EPA_Station_2006_2023_daily_O3.rds")) %>%
 
 
 ggplot(data, aes(x = year, y = rate, fill = type)) +
-  geom_area(alpha = 1, color = "white", size = 0.2) +
+  geom_area(alpha = 1, color = "white", linewidth = 0.2) +
   facet_wrap(~location, scales = "free_y") +
   scale_fill_manual(
     values = c("smoke_driven" = "#FC9272",
